dcc6cd10adc606ae4f831667fff6698aab9948cc
Ismabc05 <ismaelbedmarcejas@gmail.com>
2026-08-18 11:38:02 +0200

    crud completo con dto y entidad de categories

--- backend/src/products/controllers/categories.controller.ts
+++ backend/src/products/controllers/categories.controller.ts
@@ -1,4 +1,46 @@
 import { Controller } from '@nestjs/common';
+import {
+  Get,
+  Put,
+  Post,
+  Delete,
+  ParseIntPipe,
+  Param,
+  Body,
+} from '@nestjs/common';
+
+import { CreateCategoryDto, UpdateCategoryDto } from '../dtos/category.dto';
+import { CategoriesService } from '../services/categories.service';
 
 @Controller('categories')
-export class CategoriesController {}
+export class CategoriesController {
+  constructor(private categoriesService: CategoriesService) {}
+
+  @Get()
+  finfAll() {
+    return this.categoriesService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.categoriesService.findOne(id);
+  }
+
+  @Post()
+  create(@Body() newCategory: CreateCategoryDto) {
+    return this.categoriesService.create(newCategory);
+  }
+
+  @Put(':id')
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateCategory: UpdateCategoryDto,
+  ) {
+    return this.categoriesService.update(id, updateCategory);
+  }
+
+  @Delete(':id')
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.categoriesService.remove(id);
+  }
+}
--- backend/src/products/services/categories.service.ts
+++ backend/src/products/services/categories.service.ts
@@ -1,4 +1,62 @@
 import { Injectable } from '@nestjs/common';
 
+import { Category } from '../entities/category.entity';
+import { CreateCategoryDto, UpdateCategoryDto } from '../dtos/category.dto';
+
 @Injectable()
-export class CategoriesService {}
+export class CategoriesService {
+  private categories: Category[] = [
+    {
+      id: 1,
+      name: 'Laptop Lenovo IdeaPad',
+    },
+    {
+      id: 2,
+      name: 'iPhone 15',
+    },
+  ];
+
+  findAll() {
+    return this.categories;
+  }
+
+  findOne(id: number) {
+    const category = this.categories.find((category) => category.id === id);
+    return category;
+  }
+
+  create(newCategory: CreateCategoryDto) {
+    const category: Category = {
+      id: this.categories.length + 1,
+      ...newCategory,
+    };
+    this.categories.push(category);
+    return category;
+  }
+
+  update(id: number, updateCategory: UpdateCategoryDto) {
+    const categoryIndex = this.categories.findIndex(
+      (category) => category.id === id,
+    );
+    if (categoryIndex === -1) {
+      return null;
+    }
+
+    this.categories[categoryIndex] = {
+      ...this.categories[categoryIndex],
+      ...updateCategory,
+    };
+    return this.categories[categoryIndex];
+  }
+
+  remove(id: number) {
+    const categoryIndex = this.categories.findIndex(
+      (category) => category.id === id,
+    );
+    if (categoryIndex === -1) {
+      return null;
+    }
+
+    return this.categories.splice(categoryIndex, 1);
+  }
+}
