14ea3dc088aaf9c10bd769b7cf11bfc61192ceab
Ismabc05 <ismaelbedmarcejas@gmail.com>
2026-09-13 19:31:22 +0200

    mejora estilos login

--- frontend/src/paginas/Auth/Login.tsx
+++ frontend/src/paginas/Auth/Login.tsx
@@ -10,11 +10,13 @@ function Login() {
 
                     <h1 className="login__title">¡Hola!</h1>
 
-                    <p className="login__description">Introduce tu direccion de correo para iniciar sesion o registrarte</p>
+                    <p className="login__description">Introduce tu dirección de correo para iniciar sesión o registrarte</p>
 
                     <form className="login__form">
                         <label htmlFor="email">EMAIL:</label>
                         <input id="email" type="email" name="email" />
+                        <label htmlFor="email">CONTRASEÑA:</label>
+                        <input id="password" type="passwors" name="password" />
                         <p className="login__terms">
                             Al continuar, aceptas los términos y condiciones de Nuvé
                             y confirmas que has leído nuestra política de privacidad.
