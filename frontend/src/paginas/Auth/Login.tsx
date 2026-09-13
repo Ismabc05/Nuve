dfc384ab6ac53b7007735181b3c8c5dc52379fcf
Ismabc05 <ismaelbedmarcejas@gmail.com>
2026-09-13 12:40:46 +0200

    estilos login

--- frontend/src/paginas/Auth/Login.tsx
+++ frontend/src/paginas/Auth/Login.tsx
@@ -1,5 +1,31 @@
+import '../../estilos/auth/login.css'
+
 function Login() {
-    return <h1>Login funcionando</h1>
+    return (
+        <>
+            <main className="login">
+                <section className="login__container">
+
+                    <img className="login__logo" src="/login.png" alt="Nuvé" />
+
+                    <h1 className="login__title">¡Hola!</h1>
+
+                    <p className="login__description">Introduce tu direccion de correo para iniciar sesion o registrarte</p>
+
+                    <form className="login__form">
+                        <label htmlFor="email">EMAIL:</label>
+                        <input id="email" type="email" name="email" />
+                        <p className="login__terms">
+                            Al continuar, aceptas los términos y condiciones de Nuvé
+                            y confirmas que has leído nuestra política de privacidad.
+                        </p>
+                        <button type="submit">Continuar</button>
+                    </form>
+
+                </section>
+            </main>
+        </>
+    )
 }
 
 export default Login
